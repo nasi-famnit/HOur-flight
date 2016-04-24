10742dc3a8cb79f94536791885c9c924ea712588
Daniel Siladji <szilagyi.d@gmail.com>
2016-04-24 12:36:10 +0200

    Full app commit

---
+++ static/Skriptica1.js
@@ -0,0 +1,224 @@
+﻿var dataFromGoogle = "";
+var globalPosID = "";
+var globalDestID = "";
+function getFlightInfo(posID, destID)
+{
+var tmpPos = posID;
+posID = destID;
+destID = tmpPos;
+console.log(posID + " " + destID);
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+var yyyy = today.getFullYear();
+
+if(dd<10) {
+    dd='0'+dd
+} 
+
+if(mm<10) {
+    mm='0'+mm
+} 
+
+var datum = yyyy+'-'+mm+'-'+dd;
+var dataObject = {
+  "request": {
+    "passengers": {
+      "adultCount": 1
+    },
+    "slice": [
+      {
+        "origin": posID,
+        "destination": destID,
+        "date": datum,
+        "maxStops": 1
+      }
+    ]
+  }
+};
+
+$.ajax({
+     type: "POST",
+     //Set up your request URL and API Key.
+     url: "https://www.googleapis.com/qpxExpress/v1/trips/search?key=AIzaSyAgBrSZFSnB5xi3EekRz2XtewYo1Kguo50", 
+     contentType: 'application/json', // Set Content-type: application/json
+     dataType: 'json',
+     data: JSON.stringify(dataObject),
+     success: function (data) {
+      if(data == null || data.trips == null || data.trips.tripOption == null)
+      {
+          alert("There is no direct flights between " + posID + " and " + destID);
+          console.log("No info about your flights");
+          return;
+      }
+      dataFromGoogle = data;
+      var allFlights = data.trips.tripOption;
+      console.log(allFlights.length);
+      var validFlights = 0;
+      for(i = 0; i < allFlights.length; i=i+1)
+      {
+
+          var segments = allFlights[i].slice[0].segment;
+          //console.log(i);
+          if(segments.length == 1)
+          {
+              validFlights ++;
+              var idStr = "red".concat(i.toString());
+              $("#tableId").append("<tr id="+idStr+" class=info style='cursor:pointer;'></tr>");
+              var desc = segments[0].leg[0];
+              var temporary = desc.departureTime.split("T");
+              var printTime = "";
+              printTime += temporary[1].substring(0,5) + " (";
+              printTime += temporary[1].substring(5,temporary[1].length) + ") (" + temporary[0] +")";
+
+              $("#"+idStr).append("<td>" + printTime + "</td>");
+              $("#"+idStr).append("<td>" + desc.aircraft + "</td>");
+              $("#"+idStr).append("<td>" + segments[0].flight.carrier + "</td>");
+              $("#"+idStr).append("<td>" + segments[0].flight.number + "</td>");
+              $("#"+idStr).append("<td>" + desc.originTerminal + "</td>");
+          }
+      }
+      if(validFlights == 0)
+      {
+          alert("There is no direct flights between " + posID + " and " + destID);
+      }
+      console.log("Finished");
+    },
+      error: function(){
+       alert("Access to Google QPX Failed.");
+     }
+    });
+}
+
+
+
+function findFlights()
+{
+
+    $posID = $('#select-to1')[0].selectize.getValue();
+    $destID = $('#select-to')[0].selectize.getValue();
+    if($posID == "" && $destID == "")
+    {
+        if(globalPosID != "" && globalDestID != "")
+        {
+            var temp = globalPosID.split(" ");
+            $posID = temp[temp.length -1];
+            temp = globalDestID.split(" ");
+            $destID = temp[temp.length-1];
+            getFlightInfo($posID, $destID);
+        }
+        return;
+    }
+    else if($posID != "" && $destID == "")
+    {
+        if(globalDestID != "")
+        {
+          var temp = globalDestID.split(" ");
+          $destID = temp[temp.length-1];
+          getFlightInfo($posID, $destID);
+        }
+        return;
+    }
+    else if($posID == "" && $destID != "")
+    {
+        if(globalPosID != "")
+        {
+            var temp = globalPosID.split(" ");
+            $posID = temp[temp.length -1];
+            getFlightInfo($posID, $destID);
+        }
+    }
+    else
+    {
+        getFlightInfo($posID, $destID);
+    }
+    
+}
+
+document.onreadystatechange = function () {
+    $('table').on('click', 'tr' , function (event) {
+      var tmp = $(this).attr("id");
+      var i = parseInt(tmp.substring(3,tmp.length));
+      flightSelected(i);
+    });
+
+    $('.depBtn').click(function(){
+        globalPosID = $(this).find("p").text();
+        //$('#select-to1')[0].selectize.setValue($selectedAir);
+    });
+
+    $('.arrBtn').click(function(){
+        globalDestID = $(this).find("p").text();
+    });
+}
+
+function flightSelected(flightID)
+{
+    //alert(flightID);
+    //var id = dataFromGoogle.tripOption[flightID].slice[0].segment[0].leg[0].origin
+    var origin = dataFromGoogle.trips.tripOption[flightID].slice[0].segment[0].leg[0].origin;
+    if(!(origin in airportsDB))
+    {
+        alert("Error: There is no airport in database");
+        return;
+    }
+    var destination = dataFromGoogle.trips.tripOption[flightID].slice[0].segment[0].leg[0].destination;
+    var departureTime = dataFromGoogle.trips.tripOption[flightID].slice[0].segment[0].leg[0].departureTime;
+    var distance = dataFromGoogle.trips.tripOption[flightID].slice[0].segment[0].leg[0].mileage;
+    
+    var longitude = airportsDB[origin].lon;
+    var latitude = airportsDB[origin].lat;
+    var requestURL = "http://192.168.0.165:5000/predict?origin="+origin+
+        "&destination="+destination+"&distance="+distance+"&departureTime="+departureTime+
+        "&longitude="+longitude+"&latitude="+latitude;
+    //alert(origin + " " + destination + " " + departureTime + " " + distance + " " + longitude + " " + latitude);
+    getTimeDelay(requestURL, null);
+    
+    console.log(requestURL);
+}
+
+function getTimeDelay(requestURL, outputID)
+{
+    $.ajax({
+     type: "GET",
+     //Set up your request URL and API Key. // Set Content-type: application/json
+     dataType: 'jsonp',
+     crossOrigin: true,
+     url: requestURL, 
+     success: function (data) {
+      //$("#probabHolder").text("Probability: " + data.prob);
+      var prob = parseFloat(data.prob);
+      prob = 1-prob;
+      $("#probModal").modal('show');
+      console.log(Math.floor(prob*100));
+      if(Math.floor(prob*100) < 72)
+      {
+          $("#probHolder").addClass('progress-bar-warning');
+      }
+      else
+      {
+
+        $("#probHolder").addClass('progress-bar-info');
+      }
+      $("#probHolder").text(Math.floor(prob*100).toString()+"%");
+      $(".progress-bar").animate({width: Math.floor(prob*100).toString()+"%"}, 2500);
+      console.log("Done!");
+      //alert("Probability for delay: " + data.prob);
+    },
+      error: function(){
+       alert("Access to the system failed");
+     }
+    });
+}
+
+function findFlightsByID(flightID)
+{
+    
+}
+
+function nearestLocationDep()
+{
+
+}
+
+ 
---
+++ static/lokacijaDep.js
@@ -0,0 +1,71 @@
+﻿function getLocation() {
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        //x.innerHTML =
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+
+function showPosition(position) {
+    var latlon = position.coords.latitude + "," + position.coords.longitude;
+    var prazan = [];
+    //var img_url = "http://maps.googleapis.com/maps/api/staticmap?center="
+    //+ latlon + "&zoom=14&size=400x300&sensor=false";
+    //document.getElementById("mapholder").innerHTML = "<img src='" + img_url + "'>";
+    //alert(position.coords.latitude + "," + position.coords.longitude);
+    getJson(prazan, position.coords.latitude, position.coords.longitude);
+    document.getElementById("dep1").innerHTML = prazan[0].name + " - " + prazan[0].email;
+    document.getElementById("dep2").innerHTML = prazan[1].name + " - " + prazan[1].email;
+    document.getElementById("dep3").innerHTML = prazan[2].name + " - " + prazan[2].email;
+    document.getElementById("dep4").innerHTML = prazan[3].name + " - " + prazan[3].email;
+    document.getElementById("dep5").innerHTML = prazan[4].name + " - " + prazan[4].email;
+}
+
+function getLocation1() {
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition1, showError);
+    } else {
+        //x.innerHTML =
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+
+function showPosition1(position) {
+    var latlon = position.coords.latitude + "," + position.coords.longitude;
+    var prazanArr = [];
+    //var img_url = "http://maps.googleapis.com/maps/api/staticmap?center="
+    //+ latlon + "&zoom=14&size=400x300&sensor=false";
+    //document.getElementById("mapholder").innerHTML = "<img src='" + img_url + "'>";
+    //alert(position.coords.latitude + "," + position.coords.longitude);
+    getJson(prazanArr, position.coords.latitude, position.coords.longitude);
+    document.getElementById("arr1").innerHTML = prazanArr[0].name + " - " + prazanArr[0].email;
+    document.getElementById("arr2").innerHTML = prazanArr[1].name + " - " + prazanArr[1].email;
+    document.getElementById("arr3").innerHTML = prazanArr[2].name + " - " + prazanArr[2].email;
+    document.getElementById("arr4").innerHTML = prazanArr[3].name + " - " + prazanArr[3].email;
+    document.getElementById("arr5").innerHTML = prazanArr[4].name + " - " + prazanArr[4].email;
+}
+
+
+function showError(error) {
+    switch (error.code) {
+        case error.PERMISSION_DENIED:
+            x.innerHTML = "User denied the request for Geolocation."
+            break;
+        case error.POSITION_UNAVAILABLE:
+            x.innerHTML = "Location information is unavailable."
+            break;
+        case error.TIMEOUT:
+            x.innerHTML = "The request to get user location timed out."
+            break;
+        case error.UNKNOWN_ERROR:
+            x.innerHTML = "An unknown error occurred."
+            break;
+    }
+}
+
+
+
+
